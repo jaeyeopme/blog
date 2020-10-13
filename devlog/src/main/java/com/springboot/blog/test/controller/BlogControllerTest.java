425da11a0764fbd5fd59bd49afa1b2e8e0cda87a
jaeyeopme <jaeyeopme@gmail.com>
2020-10-14 04:42:46 +0900

    환경 설정 완료

---
+++ devlog/src/main/java/com/springboot/blog/test/controller/BlogControllerTest.java
@@ -0,0 +1,14 @@
+package com.springboot.blog.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "<h1>hello spring boot</h1>";
+    }
+
+}
