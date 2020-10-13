43c1946ec457a023a93df5bfc8875da901fb8a4b
jaeyeopme <jaeyeopme@gmail.com>
2020-10-14 07:29:35 +0900

    Entity 추가 JPA, Jackson 설정 추가

---
+++ devlog/src/main/java/com/springboot/blog/entity/User.java
@@ -0,0 +1,31 @@
+package com.springboot.blog.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @ColumnDefault("'USER_ROLE'")
+    private String role;
+
+    @Column(nullable = false, length = 45)
+    private String username;
+
+    @Column(nullable = false, length = 68)
+    private String password;
+
+    @Column(nullable = false, length = 45)
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp createAt;
+
+}
