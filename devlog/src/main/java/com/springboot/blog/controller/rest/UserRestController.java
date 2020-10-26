6718a11d603738b45deb6ff52bb1011021388ec5
jaeyeopme <jaeyeopme@gmail.com>
2020-10-26 22:42:24 +0900

    유저 삭제 수정

--- devlog/src/main/java/com/springboot/blog/controller/rest/UserRestController.java
+++ devlog/src/main/java/com/springboot/blog/controller/rest/UserRestController.java
@@ -29,9 +29,9 @@ public class UserRestController {
         return userService.update(user);
     }
 
-    @DeleteMapping("users/{id}")
-    public ResponseEntity delete(@PathVariable Long id) {
-        return userService.deleteById(id);
-    }
+//    @DeleteMapping("users/{id}")
+//    public ResponseEntity delete(@PathVariable Long id) {
+//        return userService.deleteById(id);
+//    }
 
 }
