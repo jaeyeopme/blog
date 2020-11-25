a7959a51c56b068428b9596a97eb496887f69580
jaeyeopme <jaeyeopme@gmail.com>
2020-11-25 12:09:33 +0900

    fix: index page

--- devlog/src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ devlog/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/login", "/signin", "/boards/*", "/users", "/js/**", "/css/**", "/images/**")
+                .antMatchers("/", "/login", "/signin", "/signup", "/boards/*", "/users", "/js/**", "/css/**", "/images/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
