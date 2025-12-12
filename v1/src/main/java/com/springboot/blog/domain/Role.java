b42cbd81f6dbea00e2a42879df6badcba7461790
jaeyeopme <jaeyeopme@gmail.com>
2025-12-12 14:45:07 +0900

    chore: promote v2 project to repo root

--- v1/src/main/java/com/springboot/blog/domain/Role.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.blog.domain;
-
-public enum Role {
-
-    ROLE_ADMIN, ROLE_USER
-
-}
--- v1/src/main/java/com/springboot/blog/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.blog.repository;
-
-import com.springboot.blog.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
-
